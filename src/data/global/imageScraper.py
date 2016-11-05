f05e7edfb51fba37ffdff14cb6e6677507cb5e00
Mehdi <mehdimkazi@gmail.com>
2016-11-05 13:04:32 -0700

    speaker pics

--- src/data/global/imageScraper.py
+++ src/data/global/imageScraper.py
@@ -4,9 +4,14 @@ import urllib.request
 
 with open('speakers.json') as json_data:
     d = json.load(json_data)
+    count = 0
     for year in d:
+    	if year == "2010":
+    		continue
     	for speaker in d[year]:
-    		if speaker["speaker_title"] == "Jodi Lomask/Capacitor" or speaker["speaker_title"] == "Mallika Chopra":
-    			break
+    		count += 1
+    		if speaker["speaker_title"] == "Jodi Lomask/Capacitor" or speaker["speaker_title"] == "Mallika Chopra" or speaker["speaker_title"] == "Celli@Berkeley":
+    			continue
+    		print(speaker["speaker_title"])
     		urllib.request.urlretrieve(speaker["speaker_image_uri"], speaker["speaker_title"] + ".jpg")
-    		
+    print(count)
